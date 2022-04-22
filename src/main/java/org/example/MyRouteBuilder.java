b4569f9d6e558f7e23d66c665bda286216bd5899
Gan0za <66248349+Gan0za@users.noreply.github.com>
2022-04-22 17:23:16 +0300

    Testing query

--- src/main/java/org/example/MyRouteBuilder.java
+++ src/main/java/org/example/MyRouteBuilder.java
@@ -2,27 +2,13 @@ package org.example;
 
 import org.apache.camel.builder.RouteBuilder;
 
-/**
- * A Camel Java DSL Router
- */
 public class MyRouteBuilder extends RouteBuilder {
 
-    /**
-     * Let's configure the Camel routing rules using Java code...
-     */
     public void configure() {
+        from("jetty://http://localhost:8888/greeting")
+                .log("Received a request")
+                .setBody(simple("${date:now:yyyy-MM-dd'T'HH:mm:ssZ}"));
 
-        // here is a sample which processes the input files
-        // (leaving them in place - see the 'noop' flag)
-        // then performs content based routing on the message using XPath
-        from("file:src/data?noop=true")
-            .choice()
-                .when(xpath("/person/city = 'London'"))
-                    .log("UK message")
-                    .to("file:target/messages/uk")
-                .otherwise()
-                    .log("Other message")
-                    .to("file:target/messages/others");
     }
 
 }
