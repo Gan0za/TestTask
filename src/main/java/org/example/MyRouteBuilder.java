64b887fc0abed4ba8f6706fef08ffa4b261e2de2
Gan0za <66248349+Gan0za@users.noreply.github.com>
2022-04-25 00:48:47 +0300

    Method Post

--- src/main/java/org/example/MyRouteBuilder.java
+++ src/main/java/org/example/MyRouteBuilder.java
@@ -5,10 +5,14 @@ import org.apache.camel.builder.RouteBuilder;
 public class MyRouteBuilder extends RouteBuilder {
 
     public void configure() {
-        from("jetty://http://localhost:8888/greeting")
-                .log("Received a request")
-                .setBody(simple("${date:now:yyyy-MM-dd'T'HH:mm:ssZ}"));
-
+        from("jetty://http://localhost:8888/test") //Тестовый запрос
+                .log("Received a test request. Time: ${date:now:yyyy-MM-dd' 'HH:mm:ssZ}") //Логирование
+                .setBody(simple("\n<h1>Hello world!</h1>\n")); //Вывод на старницу
+        from("jetty://http://localhost:8888/echo?httpMethodRestrict=POST") //Пост запрос
+                .convertBodyTo(String.class)
+                .log("Received a post request. Time: ${date:now:yyyy-MM-dd' 'HH:mm:ssZ}\n" +//Логирование
+                        "Message: ${body}\n")// сообщение в логе
+                .process(exchange -> exchange.getOut().setBody(exchange.getIn().getBody())); //Ответ клиенту
     }
 
 }
